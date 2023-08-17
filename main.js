5b977028ea578555c1b074bca44761e5379fee32
00julia <130668482+00julia@users.noreply.github.com>
2023-08-17 09:29:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-//declaração de variaveis 
-let operação = prompt("Informe a operação desejada: ")
-let numero1 = Number(prompt("Informe o primeiro número: "))
-let numero2 = Number(prompt("Informe o segundo número: "))
+let hora = new Date().getHours();
+let saudacao = "";
 
-//verificar a operação matemática selecionada
-if (operação == "+") {
-    let resultado = numero1 + numero2
-    // prompt ("O resultado da soma é " + resultado + ".")
-    document.write('O resultado da soma é ' + resultado + '!');
+if(hora < 12){
+    saudacao = "Bom dia";
+}else if(hora >= 12 && hora <= 18){
+    saudacao = "Boa tarde";
+}else{
+    saudacao = "Boa noite";
 }
-if (operação == "-") {
-    let resultado = numero1 - numero2
-    // console.log ("O resultado da subtração é " + resultado + ".")
-    document.write('O resultado da subtração é ' + resultado + '!');
-}if (operação == "*") {
-    let resultado = numero1 * numero2
-    // console.log ("O resultado da multiplicação é " + resultado + ".")
-    document.write('O resultado da multiplicação é ' + resultado + '!');
-}if (operação == "/") {
-    let resultado = numero1 / numero2
-    // console.log ("O resultado da divisão é " + resultado + ".")
-    document.write('O resultado da divisão é ' + resultado + '!');
-}
+console.log(saudacao)
+
+document.getElementById("resultado").innerHTML = saudacao
